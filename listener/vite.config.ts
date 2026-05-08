bd381755c06a7697528292dc56f425447be9eaf0
Norbert Huffschmid <124078844+NHuffschmid@users.noreply.github.com>
2026-05-08 21:52:24 +0200

    fixed checkout

--- listener/vite.config.ts
+++ listener/vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-const reactPianoKeyboardPath = path.resolve(__dirname, '../../react-piano-keyboard/src');
+const reactPianoKeyboardPath = process.env.REACT_PIANO_KEYBOARD_PATH
+  ? path.resolve(__dirname, process.env.REACT_PIANO_KEYBOARD_PATH)
+  : path.resolve(__dirname, '../../react-piano-keyboard/src');
 const reactPath = path.resolve(__dirname, 'node_modules/react');
 const reactDomPath = path.resolve(__dirname, 'node_modules/react-dom');
 
